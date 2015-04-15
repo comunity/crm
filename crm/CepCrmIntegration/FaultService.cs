f0004b5d16af9b8264e12d174d1e8700b044cf76
Hans Malherbe <hans.malherbe@gmail.com>
2015-04-15 08:00:00 +0200

    Updated comments

--- crm/CepCrmIntegration/FaultService.cs
+++ crm/CepCrmIntegration/FaultService.cs
@@ -10,7 +10,7 @@ namespace CepCrmIntegration
     public interface FaultService
     {
         // Should return a case identifier
-        // The contact ID is in fault.User.CrmContactId
+        // The contact ID is in fault.User.CrmContactId. If the contact does not exist in CRM a ConflictException must be thrown.
         // The EmailAddress field in the fault argument should not be used to update contact information.
         string RegisterEmailFault(EmailFaultLog fault);
 
--- crm/CepCrmIntegration/FeedbackService.cs
+++ crm/CepCrmIntegration/FeedbackService.cs
@@ -10,6 +10,7 @@ namespace CepCrmIntegration
     interface FeedbackService
     {
         // Returns case ID
+        // The contact ID is in feedback.User.CrmContactId. If the contact does not exist in CRM a ConflictException must be thrown.
         string RegisterFeedback(Feedback feedback);
 
         // Returns null if no response
